30a82591869839d800e121ad928b4f51f5689666
Kjbrown1 <timelessgenre@gmailnyc.com>
2022-12-21 00:21:06 -0500

    Added to be corrected

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
 # health-checks
-
 Scripts that check the health of my computers.
 
-GNU seems to be the best editor to edit entries.
-
-The purposse is to create a fancy list for health checks of my computer.
-
-Duplicate directory contains same file. removing file then Dir.
-
+This repo will be will be populated with lots of fancy checks.
 
 
 
--- all_checks.py
+++ all_checks.py
@@ -8,19 +8,19 @@ def check_reboot():
     """Returns True if the computer has a pending reboot."""
     return os.path.exists("/run/reboot-required")
 
-def check-disk_full(disk, min_gb, min_percent):
+def check_disk_full(disk, min_gb, min_percent):
     """Returns True if there isn't enough disk space, False otherwise."""
     du = shutil.disk_usage(disk)
     # Calculate the percentage of free space
     percent_free = 100 * du.free / du.total
     # Calculate how many free gigabytes
     gigabytes_free = du.free /2**30
-    if gigbytes_free = < min_gb or percent_free < min_percent:
+    if gigbytes_free < min_gb or percent_free < min_percent:
         return True
     return False
 
 def main():
-    if checks_reboot():
+    if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
     if check_disk_full(disk="/",min_gb=2, min_percent=10):
